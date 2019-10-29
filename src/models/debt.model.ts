0674b9f67c8b3f8f934ed522868240a72abc8f45
agnoam <agnoam.aghai.ooo.123@gmail.com>
2019-10-29 22:58:25 +0200

    user model added by scheme file

--- src/models/debt.model.ts
+++ src/models/debt.model.ts
@@ -1,51 +1,51 @@
-// import * as mongoose from "mongoose";
+import * as mongoose from "mongoose";
 // // import { ObligationModel } from "./obligation.model";
-// import { SynagogueModel } from "./synagogue.model";
+import { SynagogueModel } from "./synagogue.model";
 
 // /* Schema */
-// const collectionName: string = "debts";
-// const modelName: string = collectionName.slice(0, collectionName.length-1)+`M`;
+const collectionName: string = "debts";
+const modelName: string = collectionName.slice(0, collectionName.length-1)+`M`;
 
-// export const DebtModel = mongoose.model(
-//   modelName,
-//   new mongoose.Schema(
-//     {
-//       // personalDetails
-//       // id: { type: String, unique: true },
-//       synagogueRef: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: SynagogueModel.collection.name,
-//         required: true
-//       },
+export const DebtModel = mongoose.model(
+  modelName,
+  new mongoose.Schema(
+    {
+      // personalDetails
+      // id: { type: String, unique: true },
+      synagogueRef: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: SynagogueModel.collection.name,
+        required: true
+      },
 
-//       firstName: { type: String },
-//       lastName: { type: String },
-//       address: { type: String },
-//       phone: { type: String },
-//       email: { type: String },
-//       // parents
-//       fatherName: { type: String },
-//       motherName: { type: String },
-//       // fatherParents
-//       father_grandFatherName: { type: String },
-//       father_grandMotherName: { type: String },
-//       // motherParents
-//       mother_grandFatherName: { type: String },
-//       mother_grandMotherName: { type: String },
+      firstName: { type: String },
+      lastName: { type: String },
+      address: { type: String },
+      phone: { type: String },
+      email: { type: String },
+      // parents
+      fatherName: { type: String },
+      motherName: { type: String },
+      // fatherParents
+      father_grandFatherName: { type: String },
+      father_grandMotherName: { type: String },
+      // motherParents
+      mother_grandFatherName: { type: String },
+      mother_grandMotherName: { type: String },
 
-//       role: { type: String, default: "guest" },
-//       childrenList: [
-//         { type: mongoose.Schema.Types.ObjectId, ref: collectionName }
-//       ],
-//       obligationList: [
-//         {
-//           type: mongoose.Schema.Types.ObjectId,
-//           ref: ObligationModel.collection.name
-//         }
-//       ],
+      role: { type: String, default: "guest" },
+      childrenList: [
+        { type: mongoose.Schema.Types.ObjectId, ref: collectionName }
+      ],
+    //   obligationList: [
+    //     {
+    //       type: mongoose.Schema.Types.ObjectId,
+    //       ref: ObligationModel.collection.name
+    //     }
+    //   ],
 
-//       created_at: { type: Date, default: Date.now } // membership date
-//     },
-//     { collection: collectionName }
-//   )
-// );
+      created_at: { type: Date, default: Date.now } // membership date
+    },
+    { collection: collectionName }
+  )
+);
